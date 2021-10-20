4149b952c43f8793ffa823453089cbb6c2823ae6
hdogrul <hdogrul@MacBook-Pro.local>
2021-10-20 12:42:27 -0400

    adding models for the mongodb

---
+++ server/models/Message.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const messageSchema = new Schema(
+  {
+    messageText: {
+      type: String,
+      required: "Message is required",
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const Message = model("Message", messageSchema);
+
+module.exports = Message;
