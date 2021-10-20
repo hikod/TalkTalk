0a246e467d3d38849c0fef52dc914256d6d7e8ee
hdogrul <hdogrul@MacBook-Pro.local>
2021-10-20 13:36:32 -0400

    adding graphql resolvers and updating the connection js for mongo db

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,40 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    messages: [Message]
+  }
+
+  type Message {
+    _id: ID
+    messageText: String
+    createdAt: String
+    username: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    messages(username: String): [Message]
+    message(_id: ID!): Message
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addMessage(messageText: String!): Message
+    addFriend(friendId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
